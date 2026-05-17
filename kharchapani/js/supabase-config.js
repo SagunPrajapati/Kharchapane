37c7787fc501e2f3b5bc97c97ed362fea17ae039
Sagun Prajapati <65895559+SagunPrajapati@users.noreply.github.com>
2026-05-17 23:34:30 +0545

    Update supabase-config.js

--- kharchapani/js/supabase-config.js
+++ kharchapani/js/supabase-config.js
@@ -6,7 +6,7 @@
 
 
 const SUPABASE_URL = 'https://kqqwpvavmusyjvfzibbh.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_KKSgWrAw_lKR4n0QpH9RYg_CQ7J6jSG'; // paste your sb_publishable_... key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxZmFhdGZieWhnZXVlcHRieXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjEzNDMsImV4cCI6MjA5MzczNzM0M30.5roD8PA1_SXesgFKywfDnv3TbDeCrtzDQ9WH6KzJ6nc'; // paste your sb_publishable_... key
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
